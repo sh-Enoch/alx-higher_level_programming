cc8acd6ab598506b1b33682db3a76b03db46801c
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-09 13:00:03 +0000

    no c

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    letter_to_remove = "C"
-    letter = "c"
-    new =''.join(char for char in my_string if char != letter_to_remove and char != letter)
+    new ='' .join(char for char in my_string if char != "C" and char != "c")
     return new
