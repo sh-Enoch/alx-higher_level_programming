e2d2130a07758e080c204560d18614bb845e0f8f
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-11 21:24:39 +0000

    delete element

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -3,5 +3,5 @@ def simple_delete(a_dictionary, key=""):
     try:
         del a_dictionary[key]
         return a_dictionary
-    except:
-        return a_dictionary    
+    except TypeError:
+        return a_dictionary
