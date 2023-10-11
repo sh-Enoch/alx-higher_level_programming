c3d9d221208771e1e908850711ae966566ed82af
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-11 21:36:52 +0000

    delete key

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def simple_delete(a_dictionary, key=""):
-    try:
+    if key in a_dictionary:
         del a_dictionary[key]
-        return a_dictionary
-    except:
-        return a_dictionary
+    return a_dictionary
