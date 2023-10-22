14ec54a4d6157c22cf1759222bf43213242d12fb
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-22 16:46:10 +0000

    lower to upper

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for i in len(str):
-        a = ord(i)
+    result = ''
+    for i in str:
+        val = ord(i)
+        if val >= 97 and val <=122:
+            val -= 32
+        result += chr(val)
+    print("{}\n".format(result), end="")
