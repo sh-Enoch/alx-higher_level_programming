36002a0b7489884f98699f42ed79645cb68aafe7
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-23 21:35:39 +0000

    list print

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for item in my_list:
-        if count >= x:
-            break
+    for item in range(x):
         try:
-            print(item, end=' ')
+            print(my_list[item], end='')
             count += 1
-        except:
-            pass
-    print()
+        except IndexError:
+            break
+    print('')
     return count
