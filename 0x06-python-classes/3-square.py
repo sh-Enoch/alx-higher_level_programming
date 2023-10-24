b4e8a296da3ef596b749753c250e9e17dc24e33e
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-24 22:44:49 +0000

    area method

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -8,7 +8,7 @@
 
 class Square:
     """initialization """
-    def __innit__(self, size=0):
+    def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
