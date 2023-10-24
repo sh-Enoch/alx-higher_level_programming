d8f16e53c9983697f96f11b5652f0181b6004033
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-24 22:41:25 +0000

    area method

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""class square that defines a aquare
+   instantiation
+   size of type int
+   size must be > 0
+   public instance method returns square area"""
+
+
+class Square:
+    """initialization """
+    def __innit__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        a = self.__size * self.__size
+        return a
