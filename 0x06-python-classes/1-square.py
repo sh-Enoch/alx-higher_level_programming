f2c824edf566a0f53711d39116b83a0586273ead
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-10-24 21:40:55 +0000

    private size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class square that defines a square
+    private attribute size
+    instatiation with size """
+
+
+class Square:
+    """ Square class """
+    def __init__(self, size):
+        """ Initialize class """
+        self.__size = size
