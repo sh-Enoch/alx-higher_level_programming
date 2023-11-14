b366078bb70c2524e21deffb2d08590586cd9b84
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-14 23:55:43 +0000

    add __str__

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -86,8 +86,5 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return result
         else:
-            for i in range(self.__height):
-                for j in range(self.__width):
-                    result += "#"
-                result += "\n"
+            result += "\n".join("#" * self.__width for j in range(self.__height))
         return result
