54e774990b7abf49430c5827dd67b1eb55499639
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-18 19:32:26 +0000

    documented

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Function to add 2 numbers."""
+
+
 def add_integer(a, b=98):
     """Add two integers a and b.
-
+ 
     Args:
        a: First integer.
        b: Second integer.
@@ -11,8 +14,8 @@ def add_integer(a, b=98):
 
     Raises:
        TypeError: if a or b are not integers or floats.
-       """
-    if not isinstance(a , (int, float)):
+    """
+    if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
