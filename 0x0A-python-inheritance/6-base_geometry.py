82bc56b44054c50c835560e071ee8774f535c3d0
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-26 20:28:27 +0000

    add area

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Define class BaseGeometry."""
+
+    def area(self):
+        """Raise an exception."""
+        raise Exception("area() is not implemented.")
