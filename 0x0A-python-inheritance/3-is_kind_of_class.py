c2886d20b62ab21ae4e1665d0be5a0520edf80f8
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-26 16:12:21 +0000

    check type

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if obj is instance of."""
+
+
+def is_kind_of_class(obj, a_class):
+    """Check if isnstance of class specified."""
+    if isinstance(obj, a_class):
+        return True
+    return False
