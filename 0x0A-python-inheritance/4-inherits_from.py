35bafd7d7aef22320afd467d214f100e247dc5be
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-26 16:56:36 +0000

    base Geometry

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Check if is an instance of a class."""
+
+
+def inherits_from(obj, a_class):
+    """Check if an object is instance of a class."""
+    if isinstance(obj, a_class):
+        return True
+    return False
