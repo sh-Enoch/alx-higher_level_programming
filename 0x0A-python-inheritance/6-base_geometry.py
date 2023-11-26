379c05fe90fa65a35a55a3b461fe9341edff01a8
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-11-26 20:30:41 +0000

    add area

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -7,4 +7,4 @@ class BaseGeometry:
 
     def area(self):
         """Raise an exception."""
-        raise Exception("area() is not implemented.")
+        raise Exception("area() is not implemented")
