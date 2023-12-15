fa1caa2f8e50074968e39ebfb78239e2da41efde
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-15 22:48:51 +0000

    show grants

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- Lists all privileges.
-SHOW GRANTS FOR 'user_0d_1'@':localhost' IF EXISTS;
-SHOW GRANTS FOR 'user_0d_2'@l'ocalhost' IF EXISTS;
+SHOW GRANTS FOR 'user_0d_1'@':localhost';
+SHOW GRANTS FOR 'user_0d_2'@'ocalhost';
