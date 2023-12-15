c35ca300159e3a0417396e356d1ac43efb927f87
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-15 13:27:02 +0000

    create table

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,5 @@
 -- Create a new table in database hbtn_0c_0.
-CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score) VALUES(1, "John", 3);
 INSERT INTO second_table (id, name, score) VALUES(2, "Alex", 3);
 INSERT INTO second_table (id, name, score) VALUES(3, "Bob", 14);
