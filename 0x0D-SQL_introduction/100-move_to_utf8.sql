5758111919e0aef17ecc8f28b01858213fe5a984
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-16 00:53:50 +0000

    to utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
--- Coinvert bhbtn_0c_c to UTF8
-ALTER DATABASE hbtn_0c_0 
+-- Coinvert bhbtn_0c_c to UTF8 
 USE hbtn_0c_0
 ALTER TABLE first_table
 CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
