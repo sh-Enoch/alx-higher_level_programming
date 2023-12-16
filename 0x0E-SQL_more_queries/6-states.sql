d61bf6b0dc714183102358a2482afbea84e7a760
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-16 00:08:03 +0000

    auto_increment states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,7 @@
 -- Create database hbtn_0d_usa and table states.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states(PRIMARY KEY(id), id INT AUTO_INCREMENT NOT NULL, name VARCHAR(256) NOT NULL);
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+	PRIMARY KEY(id), 
+	id INT AUTO_INCREMENT NOT NULL,
+	name VARCHAR(256) NOT NULL
+);
