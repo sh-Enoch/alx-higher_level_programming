c497b4f46c28dfab60f8489d850e8724a6589d0b
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-16 00:22:51 +0000

    add foreign key to cities

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,7 @@
 -- Creates database hbtn_0d_usa and table cities.
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities(
+	PRIMARY KEY(id),
 	id INT AUTO_INCREMENT NOT NULL,
 	state_id INT NOT NULL,
 	name VARCHAR(256) NOT NULL,
