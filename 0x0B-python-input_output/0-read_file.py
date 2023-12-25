e93878d468d39193fe6738b547da93d0770a1a77
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-25 13:37:19 +0000

    file 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-def read_file(filename=""):
-    """ reads a text file"""
+"""Read a file."""
 
+
+def read_file(filename=""):
+    """Read a text file."""
     with open(filename, 'r', encoding="utf-8") as file:
         data = file.read()
         print(data)
