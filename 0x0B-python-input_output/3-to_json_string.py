73afc65fa610637fe969784eb44ef963c6cfa56d
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-25 15:37:26 +0000

    to json

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-import json
 """Returns JSON representation of an object."""
+import json
 
 
 def to_json_string(my_obj):
-    """Converts to JSON."""
+    """Convert to JSON."""
     return json.dumps(my_obj)
