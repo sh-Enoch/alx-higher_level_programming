87498d461da4987ce4e2bbf2989a4507b1eb47e7
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-25 15:03:26 +0000

    write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -2,6 +2,8 @@
 """Appends a string at the end of atext file."""
 
 
-def append_write(file="", text=""):
+def write_file(filename="", text=""):
     """Append a string."""
-    
+    with open(filename, "w", encoding="UTF8") as f:
+        f.write(text)
+    return len(text)
