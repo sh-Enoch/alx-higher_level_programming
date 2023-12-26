197333e2554044a483f0ee50526d75bc3426e4c6
sh-Enoch <wekesawafula.enoch@gmail.com>
2023-12-26 14:53:44 +0000

    dictionary representation of an object

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,6 +2,7 @@
 """Returns the dictionary description with a simple data structure."""
 import json
 
+
 def class_to_json(obj):
     """Ruturn the dictionary description with simple data structure."""
     return json.dumps(obj)
