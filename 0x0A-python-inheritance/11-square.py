03228f4fe02e5d0a91496e7f6e6b686c0aee1fff
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-07 23:14:48 +0000

    implement the __str__

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Define class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Define class Square."""
+
+    def __init__(self, size):
+        """Initialize the class."""
+        self.integer_validator('size', size)
+        self.__size = size
+
+    def area(self):
+        """Return area."""
+        return self.__size ** 2
+
+    def __str__(self):
+        """Return string representation."""
+        a =  type(self).__name__
+        return "[{}] {}/{}".format(a, self.__size, self.__size)
