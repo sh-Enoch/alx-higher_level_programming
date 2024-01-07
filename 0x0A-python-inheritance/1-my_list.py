afdd69442df69ab734994a76e229fb296ba4d45a
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-07 13:08:30 +0000

    inherit list and sort

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,9 +4,9 @@
 
 class MyList(list):
     """inherits from class list."""
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
 
     def print_sorted(self):
         """Sort list in ascending."""
-        print(sorted(self))
+        copy_list = self.copy()
+        copy_list.sort()
+        print(copy_list)
