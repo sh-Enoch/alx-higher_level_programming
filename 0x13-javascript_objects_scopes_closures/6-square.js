b87ef62e9724c46216fd57ff71b12a72a4fd8699
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-11 00:17:12 +0000

    class square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -7,7 +7,7 @@ module.exports = class Square extends require('./5-square.js') {
   charPrint (c) {
     if (c === undefined) {
       this.print();
-    }else {
+    } else {
        for (let i = 0; i < this.height; i++) console.log(c.repeat(this.width));
     }
   }
