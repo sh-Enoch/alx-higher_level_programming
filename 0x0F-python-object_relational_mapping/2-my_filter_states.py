ea3e923af3c108f90777826036f5ffa7b6fdf616
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-15 11:18:24 +0000

    match words

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Script that takes an argument and displays all values in states."""
-import sys
-import MySQLdb
+"""
+Script that takes in an argument and displays all values in.
 
+The states table of hbtn_0e_0_usa where name matches the argument.
+"""
+
+import MySQLdb
+from sys import argv
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    """ Open database connection. """
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=password, db=database_name)
+# Better visualize of args organization
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
+    name_to_search = argv[4]
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
     cursor = db.cursor()
-    query = ("SELECT * FROM states WHERE name LIKE BINARY '%{}%'
-             ORDER BY id ASC".format(state_name))
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY '%{}%' ORDER BY id ASC".format(name_to_search))
 
-    results = cursor.fetchall()
-    for result in results:
-        print(result)
+    all_rows = cursor.fetchall()
+    for row in all_rows:
+        print(row)
     cursor.close()
-    db.close()
