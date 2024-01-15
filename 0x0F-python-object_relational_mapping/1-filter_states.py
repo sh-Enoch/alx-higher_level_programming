d40e849f6a43f3ec5069bcbe5bc9810503a1c61d
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-15 00:32:15 +0000

    search for n

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""Script that lists all statesfrom dbase hbtn_0e_0_usa."""
+"""Script that lists all states with a name starting with N (upper N)."""
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    """Open database connection."""
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                          passwd=argv[2], db=argv[3])
     cursor = db.cursor()
-    query = ("SELECT * FROM states WHERE name LIKE BINARY 'N%'
-             ORDER BY id ASC")
-    cursor.execute(query)
+    cursor.execute("SELECT * FROM states\
+    WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
 
-    rows = cursor.fetchall()
-    for row in rows:
+    all_rows = cursor.fetchall()
+    for row in all_rows:
         print(row)
     cursor.close()
     db.close()
