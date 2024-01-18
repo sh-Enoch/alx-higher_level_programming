93bb9fc36452f99702ee90c0462001f652175937
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-01-18 23:25:11 +0000

    state and city

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,22 +1,26 @@
 #!/usr/bin/python3
-"""Script that prints all cities from the database."""
+"""Script that lists all cities from the database hbtn_0e_4_usa."""
 import MySQLdb
-import sys
+from sys import argv
 
-if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database_name = sys.argv[3]
+if __name__ == "__main__":
+    """ Open database connection """
+    mysql_username = argv[1]
+    mysql_password = argv[2]
+    database_name = argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, username=username,
-                         passwd=password, db=database_name)
+    db = MySQLdb.connect(host="localhost", port=3306, user=mysql_username,
+                         passwd=mysql_password, db=database_name)
     cursor = db.cursor()
-    query = ("SELECT cities.id states.name cities.name FROM states cities WHERE
-             cities.state_id=state.id ORDER BY cities.id ASC")
+
+    query = "SELECT cities.id, cities.name, states.name\
+    FROM cities, states\
+    WHERE cities.state_id = states.id\
+    ORDER BY cities.id ASC"
     cursor.execute(query)
 
-    all_cities = cursor.fetchall()
-    for row in all_cities:
+    all_rows = cursor.fetchall()
+    for row in all_rows:
         print(row)
     cursor.close()
     db.close()
