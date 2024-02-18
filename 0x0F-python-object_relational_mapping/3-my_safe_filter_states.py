c82e4a7862dc8642aad8c67ada23bde7a4959b3d
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-18 21:39:08 +0000

    sstate_

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,24 +1,29 @@
 #!/usr/bin/python3
-"""Escape SQL injection."""
-import MySQLdb
-import sys
+"""
+script that takes in an argument and displays all values in
+the states table of hbtn_0e_0_usa where name matches the
+argument safe from sql injection
+"""
 
+import sys
+import MySQLdb
 
 if __name__ == "__main__":
-    uname = sys.argv[1]
-    pas = sys.argv[2]
-    dbase = sys.argv[3]
-    state_ = sys.argv[4]
 
-    db = MySQLdb.connect(host='localhost', port=3306, username=uname,
-                         passwd=pas, database=dbase)
-    cur = db.cursor()
-    query = ("SELECT * FROM state WHERE name LIKE BINARY \'{}\'
-             ORDER BY id ASC".format(state_))
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+    state_search = sys.argv[4]
 
-    cur.execute(query)
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+    conn = MySQLdb.connect(host="localhost", port=3306, user=username,
+                           passwd=password, db=db_name, charset="utf8")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s ORDER BY id ASC",
+                (state_search, ))
+    query_rows = cur.fetchall()
+
+    for row in query_rows:
+        print(row)
+
     cur.close()
-    db.close()
+    conn.close()
