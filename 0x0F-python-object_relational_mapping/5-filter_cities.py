9c9384e0215b5d8a78128dbc6fffa745c14e00c8
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-20 06:51:54 +0000

    filter cities by state

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     cur.execute("SELECT cities.name\
                 FROM cities\
                 JOIN states ON cities.state_id=states.id\
-                WHERE states.name = %s ORDER BY\
+                WHERE states.name LIKE BINARY %s ORDER BY\
                 cities.id ASC",(state_n,))
     data = cur.fetchall()
     line =[]
