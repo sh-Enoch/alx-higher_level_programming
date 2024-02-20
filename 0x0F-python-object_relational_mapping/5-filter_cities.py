5dee1c334b4a9057ed616ea08dd19b030cbfe444
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-20 06:49:22 +0000

    filter cities

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(host='localhost', port=3306, username=uname,
                          passwd=passw, db=dbase)
     cur = db.cursor()
-    cur.execute("SELECT cities.namei\
+    cur.execute("SELECT cities.name\
                 FROM cities\
                 JOIN states ON cities.state_id=states.id\
                 WHERE states.name = %s ORDER BY\
