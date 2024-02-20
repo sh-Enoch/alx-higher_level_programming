04aadaa2d4f4dd2294f1b8da986cd88d0462737b
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-20 06:40:41 +0000

    filter cities

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Takes state name and list all cities."""
+
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    uname = sys.argv[1]
+    passw = sys.argv[2]
+    dbase = sys.argv[3]
+    state_n = sys.argv[4]
+    db = MySQLdb.connect(host='localhost', port=3306, username=uname,
+                         passwd=passw, db=dbase)
+    cur = db.cursor()
+    cur.execute("SELECT cities.namei\
+                FROM cities\
+                JOIN states ON cities.state_id=states.id\
+                WHERE states.name = %s ORDER BY\
+                cities.id ASC",(state_n))
+    data = cur.fetchall()
+    for row in data:
+        print(row)
+    cur.close()
+    db.close()
