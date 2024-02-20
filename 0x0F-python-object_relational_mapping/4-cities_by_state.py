bbf95c7fc7c5b05072e01dc3b0e564047c089c23
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-20 06:05:12 +0000

    state  by city

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
-"""Script that lists all states with a name starting with N."""
-
+"""Script that lists all cities from the database hbtn_0e_4_usa."""
 
 import sys
 import MySQLdb
-
 
 if __name__ == "__main__":
 
     username = sys.argv[1]
     password = sys.argv[2]
     db_name = sys.argv[3]
-    st = sys.argv[4]
 
     conn = MySQLdb.connect(host="localhost", port=3306, user=username,
                            passwd=password, db=db_name, charset="utf8")
     cur = conn.cursor()
-    new = ('SELECT C.id, C.name, state.name
-           FROM cities AS C
-           JOIN states ON C.states_id=states.id ORDER BY C.id ASC')
-    cur.execute(new)
+    cur.execute("SELECT cities.id, cities.name, states.name\
+                FROM cities JOIN states\
+                WHERE cities.state_id=states.id ORDER BY id ASC")
     query_rows = cur.fetchall()
 
     for row in query_rows:
