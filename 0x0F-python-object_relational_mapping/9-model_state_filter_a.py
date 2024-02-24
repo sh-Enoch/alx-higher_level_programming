69275ba41309ed145490b5581cf3ac3d78386720
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-02-24 00:22:30 +0000

    model_state_filter

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Script to print all state with a letter a database."""
-
+"""
+Script that lists all State objects that contain letter a from database
+"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_stae import State
+from model_state import State
 
 if __name__ == "__main__":
-    engine = create_engine("mwsql+mysqldb://{}:{}@localhost/{}"
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
