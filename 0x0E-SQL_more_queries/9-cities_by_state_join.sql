6908a671cb50298151c446855195ef579babdc3d
sh-Enoch <wekesawafula.enoch@gmail.com>
2024-04-21 14:47:37 +0000

    cities by state

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,6 @@
 --lists all cities contained on the database.
 SELECT cities.id, cities.name , states.name
-FROM hbtn_0d_usa.cities
-INNER JOIN hbtn_0d_usa.states
+FROM cities
+INNER JOIN states
 ON cities.state_id = states.id
-ORDER BY hbtn_0d_usa.cities.id ASC;
+ORDER BY cities.id ASC;
